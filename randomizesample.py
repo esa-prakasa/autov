7a952a34b364ae544e761f6e9ea860dab4e92143
Esa Prakasa <esa.prakasa@gmail.com>
2021-05-04 16:47:22 +0700

    Add files via upload

--- randomizesample.py
+++ randomizesample.py
@@ -1,11 +1,15 @@
 from numpy import random
 import numpy as np
 import os
+import shutil
 
 os.system('cls')
 
-#path = r'C:\Users\INKOM06\Pictures\roadDataset\Unetpusbin\track001frames'
-path = r'C:\Users\INKOM06\Pictures\_DATASET\expdata\track001frames'
+path = r'C:\Users\Esa\Pictures\_DATASET\unetpusbin\expdata\track001frames'  ##LIPI
+#path = r'C:\Users\INKOM06\Pictures\_DATASET\expdata\track001frames'         ##Home
+
+
+targetPath = r'C:\Users\Esa\Pictures\_DATASET\unetpusbin\expdata\s200frames'
 
 fileList = os.listdir(path)
 
@@ -26,4 +30,13 @@ gFin = sorted(gFin)
 #print(gFin) 
 
 for i in range(sampSz):
+	#print("%d:  %s    %s"%(i,gFin[i], fileList[gFin[i]]))
 	print("%d:  %s    %s"%(i,gFin[i], fileList[gFin[i]]))
+	srcPath = os.path.join(path,fileList[gFin[i]])
+	savePath = os.path.join(targetPath,fileList[gFin[i]])
+	shutil.copyfile(srcPath, savePath)
+	print(srcPath)
+	print(savePath)
+	print(" ")
+
+
