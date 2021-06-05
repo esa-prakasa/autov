2b08578a48b784db9ad6816b8d6c1bff366e09b5
Esa Prakasa <esa.prakasa@gmail.com>
2021-06-05 08:41:26 +0700

    Add files via upload

---
+++ cmattopAll.py
@@ -0,0 +1,99 @@
+import cv2
+import os
+import numpy as np
+
+# confusion matrix in sklearn
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import classification_report
+
+os.system('cls')
+
+rootPath = r'C:\Users\Esa\Pictures\_DATASET\unetpusbin\topCamSegmented\kFoldFolders\D3'   ## fold1 Train: D0-D1-D2  Test: D3
+#resPath = r'C:\Users\Esa\Pictures\_DATASET\unetpusbin\result_fold0'
+resPath = r'C:\Users\Esa\Pictures\_DATASET\unetpusbin\result_fold0B'
+
+
+folderNames = os.listdir(rootPath)
+resNames = os.listdir(resPath)
+
+NTest = len(os.listdir(resPath))
+
+
+print("No FileName        tp       fn   fp    tn      acc    pre   rec   f1s")
+f = open(r"C:\Users\Esa\Pictures\_DATASET\unetpusbin\cmatrec\fold0.txt", "w")
+for idx in range(NTest):
+	fullPath0 = rootPath+"\\"+folderNames[idx]+"\\mask\\" #_mask.png"
+	maskFiles = os.listdir(fullPath0)
+	fullPath0 = fullPath0+maskFiles[0]
+	#print(fullPath0)
+
+	fullPath1 = resPath+"\\"+ folderNames[idx]+".png" #resNames[idx]
+	#print(fullPath1)
+
+	img0 = cv2.imread(fullPath0)
+	M = int(0.5*img0.shape[0])
+	N = int(0.5*img0.shape[1])
+
+	img0 = cv2.resize(img0, (N,M), interpolation = cv2.INTER_AREA)
+	img1 = cv2.imread(fullPath1)
+	img1 = cv2.resize(img1, (N,M), interpolation = cv2.INTER_AREA)
+
+
+	actual = []
+	predicted = []
+
+	for i in range(M):
+		for j in range(N):
+			if img0[i,j,0] >= 200:
+				val = 1
+			if img0[i,j,0] < 200:
+				val = 0
+			actual.append(val)
+			if img1[i,j,0] >= 200:
+				val = 1
+			if img1[i,j,0] < 200:
+				val = 0
+			predicted.append(val)
+
+	matrix = confusion_matrix(actual,predicted, labels=[1,0])
+#print('Confusion matrix : \n',matrix)
+
+	tp, fn, fp, tn = confusion_matrix(actual,predicted,labels=[1,0]).reshape(-1)
+
+	acc = (tp+tn)/(tp+tn+fp+fn)
+	pre = tp/(tp+fp)
+	rec = tp/(tp+fn)
+	f1s = 2*(pre*rec)/(pre+rec)
+	
+
+
+#	print("No FileName        tp       fn   fp    tn      acc    pre   rec   f1s")
+	resultText = ("%d %s %d %d %d %d %3.2f %3.2f %3.2f %3.2f \n"%(idx, folderNames[idx],tp, fn, fp, tn, acc, pre, rec, f1s))
+	print(resultText)
+	f.write(resultText)
+
+# classification report for precision, recall f1-score and accuracy
+	#matrix = classification_report(actual,predicted,labels=[1,0])
+	#print('Classification report : \n',matrix)
+
+f.close()
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
